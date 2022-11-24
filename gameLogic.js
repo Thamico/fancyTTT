4955b3f6161946cbab9a6e82699f884372a6c82c
Thamico <thanh.nguyenctn1999@gmail.com>
2022-11-24 14:58:04 +0100

    removed start button and added draw check

--- gameLogic.js
+++ gameLogic.js
@@ -14,52 +14,78 @@ var symbol = playerOne.symbol;
 
 var { board } = gameBoard;
 
+var { resetBoard } = gameBoard;
+
 function mark(e) {
   e.innerHTML = this.symbol;
 
   this.board[parseInt(e.id)] = this.symbol;
 
+  playerTurn(this.symbol);
+
+  winCheck(this.board, this.symbol);
+}
+
+function playerTurn(symbol) {
   if (this.symbol === playerOne.symbol) {
     this.symbol = playerTwo.symbol;
   } else this.symbol = playerOne.symbol;
-
-  winCheck(this.board, this.symbol);
 }
 
 function winCheck(board, symbol) {
   if (board[0] === board[1] && board[1] === board[2] && board[0] !== "") {
     winMessage(symbol);
-    reset();
-  }
-  if (board[3] === board[4] && board[4] === board[5] && board[3] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[3] === board[4] &&
+    board[4] === board[5] &&
+    board[3] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[6] === board[7] && board[7] === board[8] && board[6] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[6] === board[7] &&
+    board[7] === board[8] &&
+    board[6] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[0] === board[3] && board[3] === board[6] && board[0] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[0] === board[3] &&
+    board[3] === board[6] &&
+    board[0] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[1] === board[4] && board[4] === board[7] && board[1] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[1] === board[4] &&
+    board[4] === board[7] &&
+    board[1] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[2] === board[5] && board[5] === board[8] && board[2] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[2] === board[5] &&
+    board[5] === board[8] &&
+    board[2] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[0] === board[4] && board[4] === board[8] && board[0] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[0] === board[4] &&
+    board[4] === board[8] &&
+    board[0] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (board[2] === board[4] && board[4] === board[6] && board[2] !== "") {
+    reset(board, symbol);
+  } else if (
+    board[2] === board[4] &&
+    board[4] === board[6] &&
+    board[2] !== ""
+  ) {
     winMessage(symbol);
-    reset();
-  }
-  if (
+    reset(board, symbol);
+  } else if (
     board[0] !== "" &&
     board[1] !== "" &&
     board[2] !== "" &&
@@ -71,6 +97,7 @@ function winCheck(board, symbol) {
     board[8] !== ""
   ) {
     alert("Draw");
+    reset(board, symbol);
   }
 }
 
@@ -80,7 +107,21 @@ function winMessage(symbol) {
   } else alert("Player One won!");
 }
 
-function reset() {
-  const { board } = gameBoard;
-  var symbol = playerOne.symbol;
+function reset(board, symbol) {
+  clearArray(board);
+  symbol = playerOne.symbol;
+
+  console.log(board);
+  console.log(symbol);
+
+  const field = document.getElementById("board");
+  for (const child of field.children) {
+    child.replaceChildren();
+  }
+}
+
+function clearArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = "";
+  }
 }
